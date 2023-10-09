ab0d07562344bd118aba6468c34030066879672b
LlagosteraPol <llagosterapol.work@gmail.com>
2023-10-09 14:21:57 +0200

    Added more info

--- Interface/customtkinter_complex.py
+++ Interface/customtkinter_complex.py
@@ -23,7 +23,9 @@ class App(customtkinter.CTk):
 
         # create sidebar frame with widgets
         self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
-        self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
+        #https://www.tutorialspoint.com/python/tk_grid.htm
+        self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew") ## Add weight property to span the widget in remaining space
+        #https://www.plus2net.com/python/tkinter-rowconfigure.php
         self.sidebar_frame.grid_rowconfigure(4, weight=1)
         self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="CustomTkinter", font=customtkinter.CTkFont(size=20, weight="bold"))
         self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
